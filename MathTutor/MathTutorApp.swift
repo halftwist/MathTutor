e147123ee11abac855dcfb1fcf681570605e60cb
John Kearon <jkearon@hotmail.com>
2025-04-03 14:14:22 -0400

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by John Kearon on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
